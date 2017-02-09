cb2c09382d3f142380f0591230e611f7914a1815
rentalname <agenda21st@gmail.com>
2017-02-09 18:08:32 +0900

    [from now] 2017/02/09 18:08:32
    diff --git a/lib/kouzo.rb b/lib/kouzo.rb
    index c06cef8..c4e1bb1 100644
    --- a/lib/kouzo.rb
    +++ b/lib/kouzo.rb
    @@ -1,5 +1,17 @@
     require "kouzo/version"
    
     module Kouzo
    -  # Your code goes here...
    +  class Node < Struct.new(:parent, :val)
    +    attr_reader :children
    +    def initialize(parent, val)
    +      @children = []
    +    end
    +
    +    def add(node, &call_in)
    +      @children << node
    +      yield node
    +    end
    +  end
    +
    +
     end

--- lib/kouzo.rb
+++ lib/kouzo.rb
@@ -1,5 +1,17 @@
 require "kouzo/version"
 
 module Kouzo
-  # Your code goes here...
+  class Node < Struct.new(:parent, :val)
+    attr_reader :children
+    def initialize(parent, val)
+      @children = []
+    end
+
+    def add(node, &call_in)
+      @children << node
+      yield node
+    end
+  end
+
+
 end
